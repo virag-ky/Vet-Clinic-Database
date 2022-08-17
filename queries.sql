a1e6feefaaf45aab6f72345ab3c50286a1acbf79
Virag Kormoczy <vkormoczy@gmail.com>
2022-08-17 15:59:43 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -7,4 +7,4 @@ SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered;
 SELECT * FROM animals WHERE name != 'Gabumon';
-SELECT * FROM animals WHERE weight_kg BETWEEN 10.3 AND 17.4;
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
