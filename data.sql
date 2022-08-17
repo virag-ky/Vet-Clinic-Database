b17347269b032e3526ddb6819b982831baf5c1c7
Virag Kormoczy <vkormoczy@gmail.com>
2022-08-17 14:20:57 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,5 +1,7 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals VALUES (1, 'Agumon', 'February-03-2020', 0, 'true', 10.23);
+INSERT INTO animals VALUES (2, 'Gabumon', 'November-15-2018', 2, 'true', 8);
+INSERT INTO animals VALUES (3, 'Pikachu', 'January-7-2021', 1, 'false', 15.04);
+INSERT INTO animals VALUES (4, 'Devimon', 'May-12-2017', 5, 'true', 11);
+
